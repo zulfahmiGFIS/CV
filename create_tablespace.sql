a92b048291479e87b987e6c5779496a164372763
zulfahmi <zulfahmi@gfis.my>
2022-09-28 12:17:09 +0800

    sql create tablespace

---
+++ create_tablespace.sql
@@ -0,0 +1,6 @@
+create tablespace radat
+datafile '+DATA1/sckches/datafile/radat1.dbf' size 32G,
+'+DATA1/sckches/datafile/radat2.dbf' size 32G,
+'+DATA2/sckches/datafile/radat3.dbf' size 32G,
+'+DATA2/sckches/datafile/radat4.dbf' size 32G
+
